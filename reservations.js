db8352e15cd25b6638ea10b1972cab55f18b7233
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-04-29 19:14:16 -0600

    Basic express setup

---
+++ reservations.js
@@ -0,0 +1,38 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res){
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/api/tables", function (req, res){
+    return res.json(tables);
+});
+
+app.get("/api/waitlist", function (req, res){
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
